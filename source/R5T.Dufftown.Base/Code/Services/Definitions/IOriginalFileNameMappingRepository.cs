be2715a29bdeb3f9fc955c47ce8354e5b613c4ca
davidcoats <david@getsafetycone.com>
2022-04-27 15:29:21 -0700

    Interim changes.

---
+++ source/R5T.Dufftown.Base/Code/Services/Definitions/IOriginalFileNameMappingRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.Sparta;
+
+using R5T.T0064;
+
+
+namespace R5T.Dufftown
+{
+    /// <summary>
+    /// Read-only items.
+    /// </summary>
+    [ServiceDefinitionMarker]
+    public interface IOriginalFileNameMappingRepository : IServiceImplementation
+    {
+        Task Add(OriginalFileNameMapping mapping);
+
+        Task<bool> Exists(FileName uniqueFileName);
+
+        Task<OriginalFileNameMapping> Get(FileName uniqueFileName);
+
+        Task Delete(FileName uniqueFileName);
+    }
+}
